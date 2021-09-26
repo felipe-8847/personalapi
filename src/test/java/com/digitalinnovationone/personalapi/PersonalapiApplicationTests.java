5f181944a96efd184e97e5aa685ec304a2889e81
felipe ferreira <felipealves131351@gmail.com>
2021-09-26 11:18:19 -0300

    Criação do projeto em maven com java 11 api rest

---
+++ src/test/java/com/digitalinnovationone/personalapi/PersonalapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.personalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
